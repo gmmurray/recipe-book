78c499bf4817bfc06d8ee34d11729ae2e42d56be
gmmurray <trinilime123@live.com>
2022-01-23 14:42:39 -0500

    added PWA functionality

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,9 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
                     />
                     <link rel="icon" href="/logo_minimal.png" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/icon.png"></link>
+                    <meta name="theme-color" content="#59C3C3" />
                 </Head>
                 <body>
                     <Main />
